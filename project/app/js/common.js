d54dccef74655d1a71ced1066cb3b6fea6932d16
shumGQ <dev@shum.gq>
2017-03-18 18:17:16 +0300

    Готова механика сайта.

---
+++ project/app/js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	loop: true,
+    items: 1,
+    margin: 10,
+    center: true,
+    
+  });
+});
---
+++ project/gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglifyjs'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename');
+
+gulp.task('mytask', function() {
+	console.log('Привет!');
+});
+
+gulp.task('sass', function() {
+  return gulp.src('app/sass/**/*.+(scss|sass|css)')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+    server: {
+      baseDir: 'app'
+    },
+    notify: true
+  });
+});
+
+gulp.task('scripts', function() {
+  return gulp.src([
+    'app/libs/jquery/dist/jquery.min.js',
+    'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js'
+    ])
+  .pipe(concat('libs.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('css-libs', ['sass'], function() {
+  return gulp.src('app/css/libs.css')
+  .pipe(cssnano())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('app/css/'));
+});
+
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+  gulp.watch('app/sass/**/*.sass', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/css/style.css', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
