7c832707003ae1155375a63734095df74c6f0499
shumGQ <dev@shum.gq>
2017-03-19 10:23:45 +0300

    Header done!

--- project/app/js/common.js
+++ project/app/js/common.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
   	loop: true,
-    items: 1,
-    margin: 10,
+    items: 2,
+    margin: 0,
     center: true,
-    
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
   });
 });
