6e9c0c52c3dc33daacf7218a148fd7badb08904f
shumGQ <dev@shum.gq>
2017-03-19 15:34:54 +0300

    Desktop version, without actually photos.

--- project/app/js/common.js
+++ project/app/js/common.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
   	loop: true,
-    items: 2,
     margin: 0,
     center: true,
     autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: true,
+    smartSpeed: 1000,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: false,
+    responsive: {
+    	0: {
+    		items: 1,
+    	},
+    	600: {
+    		items: 2
+    	},
+    	1000: { 
+    		items: 3
+    	}
+    }
   });
 });
